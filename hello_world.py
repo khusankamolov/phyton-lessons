3dbe111cbd6e15b4f44b3891882b5c3cc5443867
khusankamolov <husankamolov27@gmail.com>
2025-08-18 12:44:13 +0500

    yangilandi
    14-dars qo'shildi

--- hello_world.py
+++ hello_world.py
@@ -4,30 +4,27 @@ Created on Sat Aug  9 12:17:08 2025
 
 @author: Surface
 """
-
-x = int(input("Birinchi sonni kiriting:"))
-y = int(input('Ikkinchi sonni kiriting:'))
-if x==y:
-   print(f'{x}=555{y}')
-#elif x>y:
-#    print(f'{x}>{y}')
-#elif x<y:
-    #print(f'{x}<{y}')
-#mahsulotlar = ['olma','bodring','orik','shaftoli','sut','gosht','qalampir','guruch','banan','t
-#foydalanuvchilar = ['anvar','hasan','husan','ali','bobur']
-#login = input('Yangi login kiriting:')
-#if login.lower() in foydalanuvchilar:
-#    print('Login band boshqa login tanlang')
-#elif login.lower() not in foydalanuvchilar:
-#    foydalanuvchilar.append(login)
-#    print('Xush kelibsiz,yangi foydalanuvchi')
-
-users = ['alisher1983','aziza','yasina' 'umar']
-
-login = input("Yangi login tanlang:" )
-
-if login  in users:
-    print('Login band, yangi login tanalng!')
+onam = {'ism':'feruzaxon','yosh':'52','t_joy':"farg'ona"}
+#print(f"Mening onam {onam['ism'].title()} \
+#{onam['yosh']}yoshdalar va {onam['t_joy'].title()}da tug'ilganlar.")
+oila_taom = {'onam':'mastava','dadam':'sho\'rva','hasan':'kabab','abubakr':'kchiri',
+             'sharifaxon':'osh'}
+#print(f"Onamning sevimli ovqatlari {oila_taom['onam']}")
+#print(f"Dadamning sevimli ovqatlari {oila_taom['dadam']}")
+#print(f"Akamning sevimli taomi {oila_taom['abubakr']}")
+#print(f"Hasanning sevimli taomi {oila_taom['hasan']}")
+#print(f"Opamning sevimli taomi {oila_taom['sharifaxon']}")
+i_lugat = {'integer':'butun son','float':"o'nlik son",'list':'sansh uchun',
+           'if':'agar','string':'matn','get':'olmoq',
+           'input':'konsolga chiqarish uchun','elif':"if ga qo'shimcha",
+           'else':'bunday bolmasa','append':"qo'shmoq"}
+soz = input("Kalit so'z kiriting:").lower()
+#if soz.lower() in i_lugat:
+    #print(f"Ushbu kalit so'z {soz.lower()} {i_lugat[soz.lower()].lower()} deb tarjima qilinadi")
+#else:
+  #   print("Bunday so'z mavjud emas")
+tarjima = i_lugat.get(soz)
+if tarjima == None:
+    print('Bunday so\'z mavjud emas')
 else:
-    print("Xush kelibsiz!")
-    print('we have lessons')
+    print(f"{soz.title()} sozi {tarjima.lower()} deb tarjima qilinadi.")
