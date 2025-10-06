905f95878119292e067a02c85fcea624e462e36a
khusankamolov <husankamolov27@gmail.com>
2025-10-06 13:11:45 +0500

    there are several changes.
    it is from 14-lessons.

--- hello_world.py
+++ hello_world.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug  9 12:17:08 2025
+Created on Tue Aug 19 11:27:43 2025
 
 @author: Surface
 """
-onam = {'ism':'feruzaxon','yosh':'52','t_joy':"farg'ona"}
-#print(f"Mening onam {onam['ism'].title()} \
-#{onam['yosh']}yoshdalar va {onam['t_joy'].title()}da tug'ilganlar.")
-oila_taom = {'onam':'mastava','dadam':'sho\'rva','hasan':'kabab','abubakr':'kchiri',
-             'sharifaxon':'osh'}
-#print(f"Onamning sevimli ovqatlari {oila_taom['onam']}")
-#print(f"Dadamning sevimli ovqatlari {oila_taom['dadam']}")
-#print(f"Akamning sevimli taomi {oila_taom['abubakr']}")
-#print(f"Hasanning sevimli taomi {oila_taom['hasan']}")
-#print(f"Opamning sevimli taomi {oila_taom['sharifaxon']}")
-i_lugat = {'integer':'butun son','float':"o'nlik son",'list':'sansh uchun',
-           'if':'agar','string':'matn','get':'olmoq',
-           'input':'konsolga chiqarish uchun','elif':"if ga qo'shimcha",
-           'else':'bunday bolmasa','append':"qo'shmoq"}
-soz = input("Kalit so'z kiriting:").lower()
-#if soz.lower() in i_lugat:
-    #print(f"Ushbu kalit so'z {soz.lower()} {i_lugat[soz.lower()].lower()} deb tarjima qilinadi")
+
+onam = {'ism':'feruzaxon','yosh':53,'t_yil':1974,'viloyat':'fargona'}
+#print(f"Mening onam {onam['ism'].title()} {onam['t_yil']} yilda \
+#{onam['viloyat'].title()}da tug\'ilgan va uning yoshi {onam['yosh']}da.")
+s_taomlar={'otam':'mastava','onam':'osh','akam':'kchiri','apam':'shorva',\
+           "opoqim":'lagmon'}
+#print(f"Otamning sevimli taomi {s_taomlar['otam']}.\
+#Akamning sevimli taomi {s_taomlar['akam']}.\
+#Opoqimning sevimli taomi {s_taomlar['opoqim']}.")
+#for key in s_taomlar:
+#   print(f"{key.title()}ning sevimli taomi {s_taomlar[key]}.")
+lugatlar={'integer':'butun son','float':'onlik son','if':'agar',\
+          'else':'aks holda','string':'matn','variable':'ozgaruvchi',\
+        'length':'uzunlik','append':'qoshmoq','remove':'ochirmoq','type':'tur'}
+key=input("Kalit so'zni kiriting:").lower()
+#if key.lower() in lugatlar:
+ #       print(f"Bu kalit so'zning tarjimasi {lugatlar[key.lower()]})
 #else:
-  #   print("Bunday so'z mavjud emas")
-tarjima = i_lugat.get(soz)
-if tarjima == None:
-    print('Bunday so\'z mavjud emas')
+ #      print("Bunday kalit so'z mavjud emas")      
+#value=lugatlar.get(key,"Bunday kalit so'z mavjud emas" )
+#print(value)
+tarjima=lugatlar.get(key)
+if tarjima==None:
+    print("Bizda bunday kalit so'z mavjud emas.")
 else:
-    print(f"{soz.title()} sozi {tarjima.lower()} deb tarjima qilinadi.")
+    print(f"'{key.title()}' kalit so'zning \
+tarjimasi {tarjima}.")
